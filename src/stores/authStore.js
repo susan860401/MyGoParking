1a123028d2377713ceb113f00b196b3ef7dc8a2b
yunjane <yunyun89116@gmail.com>
2024-10-26 16:21:30 +0800

    pinia

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,19 +2,41 @@ import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
-    isLogin: false, // 初始狀態
+    isLogin: false, // 初始登錄狀態
+    user: {
+      userId: null,
+      username: "",
+      phone: "",
+      email: "",
+      licensePlate: "",
+    },
   }),
   actions: {
     login() {
       this.isLogin = true;
+      localStorage.getItem("user");
+      localStorage.getItem("info");
     },
     logout() {
       this.isLogin = false;
-      localStorage.removeItem("user");
+      this.user = { userId: null, username: "", phone: "", email: "", licensePlate: "", password: "" }; // 重置用戶資料
+      localStorage.removeItem("user"); // 刪除本地用戶資料
+      localStorage.removeItem("info");
     },
     checkLoginStatus() {
       const user = localStorage.getItem("user");
-      this.isLogin = !!user; // 如果有 user，則設置為 true，否則為 false
+      this.isLogin = !!user; // 如果有用戶資料，設置為 true
     },
-  },
+    
+    // 更新用戶資料的方法
+    updateUser(data) {
+      this.user = { ...this.user, ...data }; // 更新用戶資料
+      localStorage.setItem("user", JSON.stringify(this.user)); // 更新本地存儲中的用戶資料
+      localStorage.setItem("info", JSON.stringify(this.user)); 
+    },
+    persist: {
+      enabled: true,
+      storage: sessionStorage, // 使用sessionStorage來存儲資料
+    },
+  }
 });
