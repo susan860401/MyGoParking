d1b02b117dcf9af5f1a9c8d6329bc1d68dae1cdd
Susan Kuo <susan860401@gmail.com>
2024-10-28 02:40:22 +0800

    月租暫存

--- src/views/CustomerCenter/MonthlyRentalView.vue
+++ src/views/CustomerCenter/MonthlyRentalView.vue
@@ -1,7 +1,190 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const API_URL = "https://localhost:7077/api";
+const activeButton = ref("current"); //目前在哪個頁面(當前合約還是歷史)
+const monthlyRentals = ref([]);
+const currentRental = ref([]);
+
+const loadMonthlyRental = async () => {
+  const response = await fetch(`${API_URL}/MonthlyRentals?userId=1`);
+  const datas = await response.json();
+  monthlyRentals.value = datas;
+  const today = new Date();
+  currentRental.value = monthlyRentals.value.filter((rental) => {
+    return new Date(rental.endDate) > today;
+  });
+  console.log(currentRental.value.length);
+};
+
+const loadCurrent = async () => {
+  activeButton.value = "current";
+  console.log("current");
+};
+
+const loadHistory = async () => {
+  activeButton.value = "history";
+  console.log("history");
+};
+
+const formatDate = (date) => {
+  const convertDate = new Date(date);
+  return `${convertDate.getFullYear()}-${
+    convertDate.getMonth() + 1
+  }-${convertDate.getDate()}`;
+};
+
+loadMonthlyRental();
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <div id="outside" class="container" data-aos="fade-up">
+      <div class="d-flex justify-content-between">
+        <div>
+          <button
+            class="list me-2"
+            @click="loadCurrent"
+            :class="{ active: activeButton == 'current' }"
+          >
+            <i class="fa-solid fa-paste"></i> 當前合約
+          </button>
+          <button
+            class="list"
+            @click="loadHistory"
+            :class="{ active: activeButton == 'history' }"
+          >
+            <i class="fa-solid fa-clock-rotate-left"></i> 歷史合約
+          </button>
+        </div>
+        <div>
+          <select
+            width="300px"
+            class="form-select form-select-sm mb-2"
+            aria-label=".form-select-sm example"
+          >
+            <option selected>車牌1</option>
+            <option value="">車牌2</option>
+          </select>
+        </div>
+      </div>
+
+      <!-- <img id="noDataImg" src="/src/assets/images/Nodatas.webp" alt="No Data" /> -->
+
+      <div class="accordion mt-2" id="accordionPanelsStayOpenExample">
+        <!-- 至少要顯示一個 -->
+        <div class="accordion-item">
+          <h2
+            v-if="currentRental.length"
+            class="accordion-header"
+            id="panelsStayOpen-headingOne"
+          >
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseOne"
+              aria-expanded="true"
+              aria-controls="panelsStayOpen-collapseOne"
+            >
+              #{{ currentRental[0].renId }}
+              {{ currentRental[0].lotName }}
+              <span style="font-size: 14px">
+                &nbsp (合約將於
+                {{ formatDate(currentRental[0].endDate) }} 終止)</span
+              >
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseOne"
+            class="accordion-collapse collapse show"
+            aria-labelledby="panelsStayOpen-headingOne"
+          >
+            <div class="accordion-body">
+              <div class="row">
+                <div class="col-md-8"></div>
+                <div class="col-md-4">
+                  <img
+                    class="rounded img-fluid"
+                    :src="`https://maps.googleapis.com/maps/api/staticmap?center=${currentRental[0].latitude},${currentRental[0].longitude}&zoom=18&size=600x300&markers=color:red%7Clabel:P%7C${currentRental[0].latitude},${currentRental[0].longitude}&key=AIzaSyALBHIW2HQWkmhCK-VXqGIoTVttRvMTtXo`"
+                    alt="Map of {{ currentRental[0].lotName }}"
+                    style="width: 100%; height: 100%"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          v-for="(current, index) in currentRental"
+          v-if="index !== 0"
+          class="accordion-item"
+        >
+          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseTwo"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseTwo"
+            >
+              #{{ current.renId }}
+              {{ current.lotName }}
+              <span style="font-size: 14px">
+                &nbsp (合約將於
+                {{ formatDate(current.endDate) }} 終止)</span
+              >
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseTwo"
+            class="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingTwo"
+          >
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-if="current in currentRental"></div>
+    </div>
+  </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+#outside {
+  box-shadow: -5px 0px 10px lightgrey;
+  padding: 15px;
+}
+
+/* 當前合約&歷史合約按鈕 */
+.list {
+  background-color: #f8f9fa;
+  color: gray;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  padding: 5px 10px;
+}
+
+/* 點擊按鈕時的狀態 */
+.active {
+  background-color: #f5c34e;
+  color: rgb(255, 255, 255);
+  border: none;
+  box-shadow: 3px 3px 10px lightgray;
+}
+
+#noDataImg {
+  width: 80%;
+  height: 250px;
+}
+</style>
--- src/views/CustomerCenter/ParkingRecordView.vue
+++ src/views/CustomerCenter/ParkingRecordView.vue
@@ -1,27 +1,43 @@
 <script setup>
-import { ref } from "vue";
+//表格 暫放 尚未用到
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
+
 const API_URL = "https://localhost:7077/api";
 const parkingRecords = ref([]);
+const isPhoneSize = ref(false);
+const isSmallScreen = ref(false); //用來判斷視窗大小決定顯示欄位
+const isMiddleScreen = ref(false);
+const router = useRouter();
 
 const loadParkingRecords = async () => {
   const response = await fetch(`${API_URL}/EntryExitManagements?userId=1`);
   const datas = await response.json();
   parkingRecords.value = datas;
-  console.log(parkingRecords.value);
 };
 
-const router = useRouter();
-const goToDetail = (id) => {
+//辨識視窗大小(依照視窗大小調整看到的表格欄位)
+const checkScreenSize = () => {
+  isPhoneSize.value = window.innerWidth < 450;
+  isSmallScreen.value = window.innerWidth >= 450 && window.innerWidth < 768;
+  isMiddleScreen.value = window.innerWidth >= 768 && window.innerWidth < 1200;
+};
+
+//跳轉到詳細資訊
+const viewDetails = (id) => {
   router.push(`/CustomerCenter/parking-record/${id}`);
 };
 
-loadParkingRecords();
+onMounted(() => {
+  checkScreenSize();
+  loadParkingRecords();
+  window.addEventListener("resize", checkScreenSize); // 監聽視窗大小變化
+});
 </script>
 
 <template>
   <div>
-    <div class="container">
+    <div class="container" id="outside">
       <div class="row">
         <div class="col-12 col-md-4 ps-0">
           <!-- 設定寬度 -->
@@ -36,56 +52,84 @@ loadParkingRecords();
           </select>
         </div>
       </div>
-    </div>
 
-    <div style="padding: 5px 15px">
-      <div
-        id="record"
-        v-for="record in parkingRecords"
-        :key="record.entryexitId"
-        @click="goToDetail(record.entryexitId)"
-        class="card mb-2 p-1"
-      >
-        <div
-          class="card-header fw-bold d-flex justify-content-between"
-          style="color: white; background-color: #d9cab3"
-        >
-          <span>{{ record.lotName }}</span>
-          <span>#{{ record.entryexitId }}</span>
-        </div>
-        <div class="card-body">
-          <p class="align-items-center mb-2" style="font-size: 20px">
-            <span
-              ><i class="fa-regular fa-clock"></i> {{ record.formatTime }}</span
-            >
-          </p>
-          <p class="mb-1" style="font-size: 12px">
-            <i class="fa-solid fa-car"></i>
-            進場時間
-            {{ record.entryTime }}
-          </p>
-          <p class="mb-1" style="font-size: 12px">
-            <i class="fa-solid fa-car"></i>
-            離場時間
-            {{ record.exitTime }}
-          </p>
-          <p style="text-align: end; font-size: 20px; color: #939185">
-            <i class="fa-solid fa-circle-dollar-to-slot"></i>
-            {{ record.amount }}
-          </p>
-        </div>
-      </div>
+      <!-- 表格區 -->
+      <!-- 屬性說明:show-overflow-tooltip-欄位超出寬度會顯示提示 -->
+      <el-table :data="parkingRecords" style="width: 100%" height="400">
+        <el-table-column
+          prop="lotName"
+          label="停車場名稱"
+          width="150"
+          :sortable="true"
+          show-overflow-tooltip
+          header-cell-class-name="custom-header"
+        ></el-table-column>
+        <el-table-column
+          v-if="!isPhoneSize"
+          prop="licensePlate"
+          label="車牌號碼"
+          width="105"
+          :sortable="true"
+        ></el-table-column>
+        <el-table-column
+          v-if="!isSmallScreen & !isPhoneSize"
+          prop="entryTime"
+          label="入場時間"
+          width="180"
+          :sortable="true"
+        ></el-table-column>
+        <el-table-column
+          v-if="!isMiddleScreen && !isSmallScreen && !isPhoneSize"
+          prop="exitTime"
+          label="離場時間"
+          width="180"
+          :sortable="true"
+        ></el-table-column>
+        <el-table-column
+          prop="totalMins"
+          label="停車時間(分)"
+          width="130"
+          :sortable="true"
+        ></el-table-column>
+        <el-table-column
+          prop="amount"
+          label="金額"
+          width="80"
+          :sortable="true"
+        ></el-table-column>
+        <el-table-column prop="amount" label="" width="60">
+          <template #default="scope">
+            <div @click="viewDetails(scope.row.entryexitId)" class="seeDetail">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
-<style lang="css" scoped>
-* {
-  margin: 0;
+<style lang="css">
+.seeDetail {
+  cursor: pointer; /* 確保整個 div 都能顯示手型游標 */
 }
 
-#record:hover {
-  cursor: pointer;
-  border: 3px solid #d8d2c2;
+.el-table__header {
+  background: linear-gradient(
+    to left,
+    #dfe9f3 0%,
+    white 100%
+  ) !important; /* 表頭的漸層背景 */
+  height: 40px;
 }
+
+.el-table__header th {
+  background-color: transparent !important; /* 每個 th 背景透明 */
+}
+
+.el-table tr {
+  background: transparent; /* 確保行背景透明 */
+}
+
+/* 表格樣式 */
 </style>
