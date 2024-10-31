c88a8d54b8a2d5a06f48515455b4110aeae3d62f
menghsiu <menghsiu0120@gmail.com>
2024-10-31 22:51:23 +0800

    首頁將註冊連結加上了, footer的意見回覆改成會抓pinia的資料, footer的連結還沒改

--- src/main.js
+++ src/main.js
@@ -12,10 +12,12 @@ import "element-plus/dist/index.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 const app = createApp(App);
 const pinia = createPinia();
-import { createPinia } from "pinia";
+pinia.use(piniaPluginPersistedstate)
 app.use(router);
 app.use(pinia);
 app.use(ElementPlus);
